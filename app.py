66f9f21cfb89547f765fa622083ad9ab367289ec
yeonjec88 <yeonjec88@gmail.com>
2020-08-15 17:47:15 +0900

    사이트 2개 추가

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # url 세팅 - 라우터 설정
+# @데코레이터 : 함수를 감싼다. 감싼 코드부터 실행되게끔 한다.
 @app.route('/')
 def index():
     # return "<h1>인덱스 페이지</h1>"
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/framework')
+def framework():
+    return render_template("framework.html")
+
 
 # 앱 실행
 if __name__ == "__main__":
