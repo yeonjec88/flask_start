fc431c5039467261bb38aad8260776f6dd2f4850
yeonjec88 <yeonjec88@gmail.com>
2020-08-22 16:59:58 +0900

    입력 예시 2 - 더하기 예제 만들기 프론트

--- app.py
+++ app.py
@@ -20,8 +20,11 @@ def framework():
 
 @app.route('/form')
 def form():
-    res = request.args.get('keyword') #args : arguments 입력인자. 매개변수를 입력할 때 사용하는 말
-    print(res) #args : arguments 입력인자. 매개변수를 입력할 때 사용하는 말
+    num1 = request.args.get('num1')
+    num2 = request.args.get('num2')
+    if num1 and num2:
+        total = float(num1) + float(num2)
+        print(total)
     return render_template("form.html")
 
 # 앱 실행
